489d6615e10a7c97cec2e4c11350f2a61b01f144
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-07-01 23:23:26 -0500

    Added Boards to be Displayed

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,11 +1,14 @@
 import utils from '../../helpers/utils';
 
-const boardsHeader = () => {
+const createHeader = () => {
   let domString = '';
-  domString += `<div id="boards">
+  domString += `<div id="board-header">
                 <h1>Boards</h1>
+                </div>
+                <div id="avatar">
+                <img src="src/images/profile.png" class="card-img-top" alt="...">
                 </div>`;
   utils.printToDom('#boards', domString);
 };
 
-export default { boardsHeader };
+export default { createHeader };
